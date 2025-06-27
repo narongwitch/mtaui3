54b42c31eaf41ee410bff8204931dc795ae0aeaf
narongwitcl <narongwit.clongnoi@marlo.com.au>
2025-06-27 10:29:29 +0700

    change default url to /login

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-
 const express = require('express');
 const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+// 👇 redirect root "/" ไปที่ "/login"
+app.get('/', (req, res) => {
+  res.redirect('/login');
+});
+
+// 👇 serve React app (SPA)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Typography from '@mui/material/Typography';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 
@@ -22,6 +22,7 @@ function App() {
 
         
         <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<LineLoginPage />} />
           <Route path="/mainsr" element={<MainSr />} />
           <Route path="/searchsr" element={<SearchSr />} />
